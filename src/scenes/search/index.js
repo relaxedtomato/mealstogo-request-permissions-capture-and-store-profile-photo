09a83d40b2e964c48c0131b57c8bf42a8667614e
Ram Bansal <ramnesh.bansal@gmail.com>
2020-07-03 12:36:46 -0400

    use Google Places API to show nearby restaurants

--- src/scenes/search/index.js
+++ src/scenes/search/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 import Firebase from '~/services/Firebase';
+import Google from '~/services/Google';
 import { Colors } from '~/styles';
 import SearchInput from './components/SearchInput';
 import SearchResults from './components/SearchResults';
@@ -20,13 +21,14 @@ const styles = StyleSheet.create({
 
 const Search = ({ navigation }) => {
   const [searchText, onChangeText] = useState(INITIAL_SEARCH_INPUT);
-  const [restaurants, onUpdateSearchResults] = useState(RestaurantData.results);
+  const [restaurants, onUpdateSearchResults] = useState([]);
+  const [restaurantData, onUpdateRestaurantData] = useState([]);
   const [user, onUpdateUser] = useState({});
 
   useEffect(() => {
     const unsubscribe = navigation.addListener('blur', () => {
       onChangeText(INITIAL_SEARCH_INPUT);
-      onUpdateSearchResults(RestaurantData.results);
+      onUpdateSearchResults(restaurantData);
     });
 
     return unsubscribe;
@@ -60,16 +62,32 @@ const Search = ({ navigation }) => {
     getUser();
   }, []);
 
+  useEffect(() => {
+    async function getRestaurantData() {
+      try {
+        if (user.geoLocation) {
+          const { results } = await Google.searchPlaces(user.geoLocation);
+          onUpdateRestaurantData(results);
+          onUpdateSearchResults(results);
+        }
+      } catch (fetchError) {
+        // eslint-disable-next-line
+        console.log(fetchError);
+      }
+    }
+    getRestaurantData();
+  }, [user.geoLocation]);
+
   return (
     <SafeAreaView style={styles.container}>
       <SearchInput
         onChangeText={onChangeText}
         searchText={searchText}
         onUpdateSearchResults={onUpdateSearchResults}
-        restaurantData={RestaurantData.results}
+        restaurantData={restaurantData.results || []}
       />
       <SearchResults
-        restaurants={restaurants}
+        restaurants={restaurants || []}
         openRestaurant={details =>
           navigation.navigate('RestaurantModal', { details })
         }
